1f26a67eb0535c5bcafdbfbb87186063e104c242
Colin Edwards <colin@recursivepenguin.com>
2015-02-27 17:56:17 -0600

    HIDApi for osx

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import usb.core
-import usb.util
-import sys
+import hid
+
+if not hid.enumerate(0x07ca, 0x9850):
+    raise IOError("No button connected")
+
+device = hid.device(0x07ca, 0x9850)
 
 
-dev = usb.core.find(idVendor=0x07ca, idProduct=0x9850)  # fill in your own device, of course
 
-print dev
+result = list()
+size = 23
+while size > 0:
+    count = min(size, 8)
+    buf = device.read(count)
+    if len(buf) < count:
+        raise IOError(
+            'pywws.device_cython_hidapi.USBDevice.read_data failed')
+    result += buf
+    size -= count
+print result
+
+buf = [0x01]
+
+if device.write(buf) != len(buf):
+        raise IOError(
+            'pywws.device_cython_hidapi.USBDevice.write_data failed')
